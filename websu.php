8624488e6303fa4b994bf7bdf75ec7ce6b4a9ff5
develost.com <massimiliano.bernabe@gmail.com>
2015-03-26 17:21:45 +0100

    changed main file to match project name

--- websu.php
+++ websu.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>Uploader - for internal use only</title>
+<title>Websu - for internal use only</title>
 </head>
 <body>
 <?php
@@ -23,8 +23,10 @@ function updateWebsite(){
     }
     
     if ($remoteVersion == $localVersion){
-        echo "remoteVersion" . $remoteVersion . " equal to localVersion " . $localVersion;
+        echo "remoteVersion " . $remoteVersion . " equal to localVersion " . $localVersion;
         return;
+    }else{
+        echo "Migrating from " . $localVersion . " to " . $remoteVersion . "<br>";
     }
     
     $now = date(Parameters::DATE_FORMAT);
@@ -67,7 +69,7 @@ function updateWebsite(){
     $htaccessContent.= "# ------------------------------------------------" . PHP_EOL;
     $htaccessContent.= "<IfModule mod_rewrite.c>" . PHP_EOL;
     $htaccessContent.= "    RewriteEngine On" . PHP_EOL;
-    $htaccessContent.= "    RewriteCond %{REQUEST_URI} ^/uploader.php" . PHP_EOL;
+    $htaccessContent.= "    RewriteCond %{REQUEST_URI} ^/websu.php" . PHP_EOL;
     $htaccessContent.= "    RewriteRule ^(.*)$ $1 [L]" . PHP_EOL;
     $htaccessContent.= "    RewriteCond %{REQUEST_URI} !^/" . $websiteRoot . PHP_EOL;
     $htaccessContent.= "    RewriteRule ^(.*)$ ". $websiteRoot ."$1 [L]" . PHP_EOL;
@@ -89,10 +91,11 @@ function main(){
         }
     }else{
         // present the login form
+        echo 'Websu - website uploader v 0.0.0 <br>' . PHP_EOL;
         echo '<form action="" method="post">' . PHP_EOL;
         echo 'username <input type="text" id="'.Parameters::USER_PARAM.'" name="'.Parameters::USER_PARAM.'"><br>' . PHP_EOL;
         echo 'password <input type="password" id="'.Parameters::PASSWORD_PARAM.'" name="'.Parameters::PASSWORD_PARAM.'"><br>' . PHP_EOL;
-        echo '<input type="submit" id="submit" name="submit">' . PHP_EOL;
+        echo '<input type="submit" id="submit" name="submit" value="update" >' . PHP_EOL;
         echo '</form>' . PHP_EOL;
     }
 }
