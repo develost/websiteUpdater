102e46401984cc6f4c524a930871750e23d6f510
develost.com <massimiliano.bernabe@gmail.com>
2015-04-30 14:11:59 +0200

    version

--- websu.php
+++ websu.php
@@ -34,11 +34,11 @@
 include_once "parameters.php";
 
 class Constants{
-    const WEBSU_CURRENT_VERSION = '0.0.8d';
+    const WEBSU_CURRENT_VERSION = '0.1.0';
     //const WEBSU_VERSION_CHECK_URL = 'https://raw.githubusercontent.com/develost/websu/master/version.txt';
-    const WEBSU_VERSION_CHECK_URL = 'https://www.develost.com/apps/websu/last/version';
-    //const WEBSU_FILE_URL = 'https://raw.githubusercontent.com/develost/websu/master/websu.php';
-    const WEBSU_FILE_URL = 'https://www.develost.com/apps/websu/last/file';
+    const WEBSU_VERSION_CHECK_URL = 'https://www.develost.com/apps/websu/last/version.php';
+    const WEBSU_FILE_URL = 'https://raw.githubusercontent.com/develost/websu/master/websu.php';
+    //const WEBSU_FILE_URL = 'https://www.develost.com/apps/websu/last/file';
 };
 
 /*************************************************************************************************
